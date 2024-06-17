1c910b40272f41bf2798510330986f15b5fe43ad
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-06-17 09:53:37 +0530

    added first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sample';
+
+  Enttxt!:string;
+  Entnum:number =0;
+
+  getVal(){
+    return "value";
+  }
+
+  num=3;
+  class="testing";
+
+  getName(a:string | number | boolean){
+    alert('Try something!!!')
+    console.log(a);
+  }
+
+  getData(){
+    alert("keyup!!")
+  }
+  getData2(){
+    alert("keydown!!")
+  }
+
+  getInfo(val:string){
+    this.Enttxt = val;
+  }
+
+  increment(a:string){
+    a=='plus'? this.Entnum++:this.Entnum--;
+  }
+ 
+ isdisable=true;
+  
 }
